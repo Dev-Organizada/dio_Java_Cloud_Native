69b0440d83da34c718c9659989dc600318e1642d
Dev Organizada <devorganizada@gmail.com>
2025-02-25 14:38:26 -0300

    Utilização de documentação na classe calculadora

---
+++ AnatomiaDasClasses/src/anatomiaClass/Calculadora.java
@@ -0,0 +1,25 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realizada operações matematica entre numeros inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe para desfrutar dos recursos oferecidos
+ * pelo autor.
+ * 
+ * @autor Nathaly Rodrigues
+ * @version 1.0
+ * @singe 25/02/25
+ * */
+
+package anatomiaClass;
+
+public class Calculadora {
+	/**
+	 * Este metodo é utilizado para a soma de dois numeros inteiros
+	 * @param numeroUm este é o primeiro parametro do metodo
+	 * @param numeroDois este é o segundo parametro do metodo 
+	 * @return int o resultado desse metodo é a soma de dois numeros*/
+	public int somar(int numeroUm, int numeroDois) {
+		return numeroUm + numeroDois;
+	}
+
+}
